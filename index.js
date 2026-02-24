e55254c6d3fd48b38455a008f363a97ac7afa2ed
maxferol <ferol.max@gmail.com>
2026-02-24 18:31:52 +0500

    added field

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+let field = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
 
 startGame();
 addResetListener();
 
-function startGame() {
+function startGame () {
     renderGrid(3);
 }
 
-function renderGrid(dimension) {
+function renderGrid (dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,9 +31,13 @@ function renderGrid(dimension) {
     }
 }
 
-function cellClickHandler(row, col) {
+function cellClickHandler (row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
+
+    if (field[row][col] != 0)
+        return;
+    field[row][col] = 1;
 
     if (findCell(row, col).textContent !== EMPTY) {
         return;
@@ -42,31 +51,31 @@ function cellClickHandler(row, col) {
      */
 }
 
-function renderSymbolInCell(symbol, row, col, color = '#333') {
+function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell(row, col) {
+function findCell (row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener() {
+function addResetListener () {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler() {
+function resetClickHandler () {
     console.log('reset!');
 }
 
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin() {
+function testWin () {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -77,7 +86,7 @@ function testWin() {
 }
 
 /* Ничья */
-function testDraw() {
+function testDraw () {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -90,6 +99,6 @@ function testDraw() {
     clickOnCell(2, 2);
 }
 
-function clickOnCell(row, col) {
+function clickOnCell (row, col) {
     findCell(row, col).click();
 }
